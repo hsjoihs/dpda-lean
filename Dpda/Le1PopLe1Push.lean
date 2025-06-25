f3e1d06e2398f7ad879500cfc732ee06b3047cbd
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2025-06-26 00:48:58 +0900

    Split off WobblyFn.lean

--- Dpda/Le1PopLe1Push.lean
+++ Dpda/Le1PopLe1Push.lean
@@ -4,40 +4,13 @@ import Mathlib.Data.Fintype.Basic
 import Mathlib.Data.Fintype.Prod
 import Mathlib.Data.Fintype.Option
 import Dpda.Basic
+import Dpda.WobblyFn
 
 universe u_
 
 -- Glossary:
 -- Le1P2 = (≤ 1)-pop, (≤ 1)-push
 -- IDesc = Instantaneous Description
-
-inductive WobblyFn U V
-  | want : (U → Option V) → WobblyFn U V
-  | noWant : V → WobblyFn U V
-
--- wobbly consumption
-def wob {U V} (wf : WobblyFn U V) (s : List U) : Option (V × List U) :=
-  match wf with
-  | WobblyFn.noWant v => some (v, s)
-  | WobblyFn.want f => match s with
-    | [] => none
-    | A :: t =>
-      match f A with
-      | none => none
-      | some v => some (v, t)
-
--- wobbly consumption, with the semantics that an empty stack always produces a Z0 when popped
-def wobZ {Γ V} (wf : WobblyFn (AugmentZ0 Γ) V) (β : List Γ) : Option (V × List Γ) :=
-  match wf with
-  | WobblyFn.noWant v => some (v, β)
-  | WobblyFn.want f => match β with
-    | [] =>  match f AugmentZ0.z0 with
-      | none => none
-      | some v => some (v, [])
-    | A :: γ =>
-      match f (AugmentZ0.fromΓ A) with
-      | none => none
-      | some v => some (v, γ)
 
 inductive Le1P2_Judge (Q: Type u_) (S: Type u_) (Γ: Type u_)
   | observeInput : WobblyFn S (WobblyFn (AugmentZ0 Γ) (AugmentEpsilon Γ × Q)) → Le1P2_Judge Q S Γ
