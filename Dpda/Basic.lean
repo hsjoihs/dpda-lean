822e148a6a158a99f7279e1c6066382b94479ae9
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2025-06-25 22:07:33 +0900

    Split off basic types to Basic.lean

--- Dpda/Basic.lean
+++ Dpda/Basic.lean
@@ -1,1 +1,34 @@
-def hello := "world"
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Finset.Insert
+import Mathlib.Data.Fintype.Basic
+import Mathlib.Data.Fintype.Prod
+import Mathlib.Data.Fintype.Option
+
+def lift {α} (f : α → Option α) : Option α → Option α :=
+  fun u =>
+    match u with
+    | none => none
+    | some (a: α) => f a
+
+inductive AugmentZ0 Γ where
+  | fromΓ : Γ → AugmentZ0 Γ
+  | z0 : AugmentZ0 Γ
+deriving DecidableEq
+
+def equiv_fintype {α β} (eqv : α ≃ β) [ft : Fintype α] : Fintype β :=
+  { elems := Fintype.elems.map eqv.toEmbedding,
+    complete := by intro b; simp only [Finset.mem_map_equiv, ft.complete]
+  }
+
+def augmentZ0_option_equiv {Γ} : AugmentZ0 Γ ≃ Option Γ :=
+  let toFn : AugmentZ0 Γ → Option Γ
+      | AugmentZ0.fromΓ g => some g
+      | AugmentZ0.z0 => none
+  let backFn : Option Γ → AugmentZ0 Γ
+      | none => AugmentZ0.z0
+      | some g => AugmentZ0.fromΓ g
+  { toFun := toFn, invFun := backFn,
+    left_inv := by intro a; cases a <;> rfl,
+    right_inv := by intro o; cases o <;> rfl }
+
+instance AugmentZ0.fintype {Γ} [ft : Fintype Γ]: Fintype (AugmentZ0 Γ) := equiv_fintype augmentZ0_option_equiv.symm
--- Dpda/CharPopStringPush.lean
+++ Dpda/CharPopStringPush.lean
@@ -17,6 +17,7 @@ import Mathlib.Data.Finset.Insert
 import Mathlib.Data.Fintype.Basic
 import Mathlib.Data.Fintype.Prod
 import Mathlib.Data.Fintype.Option
+import Dpda.Basic
 
 -- Glossary:
 -- CPSP = char-pop, string-push
@@ -31,28 +32,6 @@ structure CPSP_DPDA_IDesc(Q S Γ) where
   w : List S
   β : List Γ
 
-inductive AugmentZ0 Γ where
-  | fromΓ : Γ → AugmentZ0 Γ
-  | z0 : AugmentZ0 Γ
-deriving DecidableEq
-
-def equiv_fintype {α β} (eqv : α ≃ β) [ft : Fintype α] : Fintype β :=
-  { elems := Fintype.elems.map eqv.toEmbedding,
-    complete := by intro b; simp only [Finset.mem_map_equiv, ft.complete]
-  }
-
-def augmentZ0_option_equiv {Γ} : AugmentZ0 Γ ≃ Option Γ :=
-  let toFn : AugmentZ0 Γ → Option Γ
-      | AugmentZ0.fromΓ g => some g
-      | AugmentZ0.z0 => none
-  let backFn : Option Γ → AugmentZ0 Γ
-      | none => AugmentZ0.z0
-      | some g => AugmentZ0.fromΓ g
-  { toFun := toFn, invFun := backFn,
-    left_inv := by intro a; cases a <;> rfl,
-    right_inv := by intro o; cases o <;> rfl }
-
-instance AugmentZ0.fintype {Γ} [ft : Fintype Γ]: Fintype (AugmentZ0 Γ) := equiv_fintype augmentZ0_option_equiv.symm
 
 abbrev CPSP_Transition Q S Γ :=
   Q × (AugmentZ0 Γ) → CPSP_Judge Q S Γ
@@ -83,12 +62,6 @@ structure CPSP_DPDA (Q S Γ) where
   q0 : Q
   F : Finset Q
   transition : CPSP_Transition Q S Γ
-
-def lift {α} (f : α → Option α) : Option α → Option α :=
-  fun u =>
-    match u with
-    | none => none
-    | some (a: α) => f a
 
 def CPSP_DPDA.run_n_steps {Q S Γ} [Fintype Q] [DecidableEq Q] [Fintype S /- Σ -/] [Fintype Γ]
   (M: CPSP_DPDA Q S Γ) (w: List S) (n: ℕ) : Option (CPSP_DPDA_IDesc Q S Γ) :=
