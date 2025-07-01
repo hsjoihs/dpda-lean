85af1cd64c6573ce49294e8fb54824c83454c130
hsjoihs <hirotaka_satou@dwango.co.jp>
2025-07-01 21:40:43 +0900

    use `simp only`

--- Dpda/HopcroftToCPSP.lean
+++ Dpda/HopcroftToCPSP.lean
@@ -38,134 +38,151 @@ theorem Hopcroft_to_CPSP_preserves_semantics_if_transition_is_immediate {Q S Γ}
     CPSP_Judge.stepTransition
   ]
   cases h : idesc.β with
-  | nil => simp /- However, this case is absurd under our assumption -/
+  | nil => simp only [Option.map_none] /- However, this case is absurd under our assumption -/
   | cons A γ =>
-    simp [h]
+    simp only
     cases h2 : M.Δ (idesc.p, A) with
     | step f =>
       obtain ⟨ A2, γ2, h3, ⟨ a, h4⟩  ⟩ := stack_nonempty_and_transition_immediate
       rw [h3] at h
-      simp at h
+      simp only [List.cons.injEq] at h
       rw [h.left] at h4
       rw [h4] at h2
       contradiction
     | immediate a =>
       cases h3 : idesc.w with
       | nil =>
-        simp [h3]
+        simp only
         unfold Hopcroft_PreDPDA.transition
         match h4 : M.pda.transition (idesc.p, none, A) with
         | some (p, α) =>
-          simp [h4]
+          simp only [h4, Option.map_some]
           unfold Hopcroft_DPDA_IDesc.toCPSP
-          simp
+          simp only
           unfold Hopcroft_DPDA.Δ at h2
           match a with
           | none =>
-            simp
-            simp at h2
+            simp only [reduceCtorEq]
+            simp only at h2
             rw [h4] at h2
-            simp at h2
+            simp only [CPSP_Judge.immediate.injEq, reduceCtorEq] at h2
           | some (p2, α2) =>
-            simp at h2
+            simp only at h2
             rw [h4] at h2
-            simp at h2
+            simp only [
+              CPSP_Judge.immediate.injEq, Option.some.injEq, Prod.mk.injEq
+            ] at h2
             obtain ⟨ u, v ⟩ := h2
-            simp
+            simp only [
+              Option.some.injEq, CPSP_DPDA_IDesc.mk.injEq,
+              List.append_cancel_right_eq,
+              true_and
+            ]
             exact ⟨ v, u ⟩
         | none =>
-          simp [h4]
+          simp only [h4, Option.map_none]
           match a with
           | none =>
-            simp
+            simp only
           | some (p2, α2) =>
-            simp
+            simp only [reduceCtorEq]
             unfold Hopcroft_DPDA.Δ at h2
-            simp at h2
+            simp only at h2
             rw [h4] at h2
-            simp at h2
+            simp only [reduceCtorEq] at h2
       | cons head tail =>
-        simp [h3]
+        simp only
         match h4 : CPSP_Judge.immediate a with
         | CPSP_Judge.step b2 =>
-          simp [h4]
+          simp only
           unfold Hopcroft_PreDPDA.transition
           cases b2 head with
           | none =>
-            simp
+            simp only [Option.map_eq_none_iff]
             match h5 : M.pda.transition (idesc.p, none, A) with
             | some (p, α) =>
-              simp [h5]
+              simp only [h5, reduceCtorEq]
               contradiction
             | none =>
-              simp [h5]
+              simp only [h5]
               match h6 : M.pda.transition (idesc.p, some head, A) with
               | none =>
-                simp [h6]
+                simp only [h6]
               | some (p2, α2) =>
-                simp [h6]
+                simp only [h6, reduceCtorEq]
                 contradiction
           | some u =>
-            simp
+            simp only [Option.map_eq_some_iff]
             match h5 : M.pda.4 (idesc.p, none, A) with
             | none =>
-              simp [h5]
+              simp only [h5]
               contradiction
             | some (p, α) =>
               contradiction
         | CPSP_Judge.immediate a2 =>
           unfold Hopcroft_DPDA_IDesc.toCPSP
           unfold Hopcroft_DPDA.Δ at h2
-          simp at h2
+          simp only at h2
           match h5 : M.pda.transition (idesc.p, none, A) with
           | some (p, α) =>
-            simp
+            simp only [Option.map_some]
             match a2 with
             | some (u, v) =>
-              simp [h5]
+              simp only [
+                Option.some.injEq,
+                CPSP_DPDA_IDesc.mk.injEq,
+                List.append_cancel_right_eq,
+                true_and
+              ]
               rw [h5] at h2
-              simp at h2
+              simp only [CPSP_Judge.immediate.injEq] at h2
               unfold Hopcroft_PreDPDA.transition at h5
-              simp at h4
+              simp only [CPSP_Judge.immediate.injEq] at h4
               rw [← h2] at h4
-              simp at h4
+              simp only [Option.some.injEq, Prod.mk.injEq] at h4
               exact ⟨ h4.right, h4.left ⟩
             | none =>
-              simp at h4
+              simp only [CPSP_Judge.immediate.injEq] at h4
               rw [h4] at h2
               rw [h5] at h2
-              simp at h2
+              simp only [CPSP_Judge.immediate.injEq, reduceCtorEq] at h2
           | none =>
-            simp
+            simp only
             match M.pda.transition (idesc.p, some head, A) with
             | some (p, α) =>
-              simp
+              simp only [Option.map_some]
               rw [← h4, ← h2, h5]
-              simp
+              simp only
               match M.pda.transition (idesc.p, some head, A) with
               | some (p2, α2) =>
-                simp
+                simp only [
+                  Option.some.injEq,
+                  CPSP_DPDA_IDesc.mk.injEq,
+                  AugmentOneState.fromQ.injEq,
+                  List.append_cancel_right_eq,
+                  true_and
+                ]
                 rw [h5] at h2
-                simp at h2
+                simp only [reduceCtorEq] at h2
               | none =>
-                simp
+                simp only [reduceCtorEq]
                 rw [h5] at h2
-                simp at h2
+                simp only [reduceCtorEq] at h2
             | none =>
-              simp
+              simp only [Option.map_none]
               rw [← h4, ← h2, h5]
-              simp
+              simp only
               match M.pda.transition (idesc.p, some head, A) with
               | some (p2, α2) =>
-                simp
+                simp only [reduceCtorEq]
                 rw [h5] at h2
-                simp at h2
+                simp only [reduceCtorEq] at h2
               | none =>
-                simp
+                simp only
 
 lemma pair_eq {A} {B} {a: A} {b : B} {val} (h:  (a, b) = val) : b = val.2 ∧ a = val.1 := by
   cases val
-  simp at h
+  simp only [Prod.mk.injEq] at h
   exact ⟨ h.2, h.1 ⟩
 
 theorem Hopcroft_to_CPSP_preserves_semantics_single_step {Q S Γ}
@@ -178,15 +195,14 @@ theorem Hopcroft_to_CPSP_preserves_semantics_single_step {Q S Γ}
   after_step.map Hopcroft_DPDA_IDesc.toCPSP = after_step_cpsp := by
   cases h : idesc.β with
   | nil =>
-    simp [
-      Hopcroft_DPDA_IDesc.toCPSP,
-      Hopcroft_DPDA.toCPSP,
-      Hopcroft_DPDA.stepTransition,
+    simp only [
+      Hopcroft_DPDA.stepTransition, h, Option.map_none,
       CPSP_Judge.stepTransition,
-      h
+      Hopcroft_DPDA_IDesc.toCPSP,
+      Hopcroft_DPDA.toCPSP
     ] /- However, this case is absurd under our assumption -/
   | cons A γ =>
-    simp [h]
+    simp only
     cases h2 : M.Δ (idesc.p, A) with
     | immediate a =>
       have hs : (∃ A γ, idesc.β = A :: γ ∧ ∃ a, M.Δ (idesc.p, A) = CPSP_Judge.immediate a) := by
@@ -198,83 +214,82 @@ theorem Hopcroft_to_CPSP_preserves_semantics_single_step {Q S Γ}
       have h3 := Hopcroft_to_CPSP_preserves_semantics_if_transition_is_immediate M idesc hs
       apply h3
     | step f =>
-      simp [
-        Hopcroft_DPDA_IDesc.toCPSP,
-        Hopcroft_DPDA.toCPSP,
-        Hopcroft_DPDA.stepTransition,
-        CPSP_Judge.stepTransition,
-        h
-      ]
-      simp [h2]
+      simp only [Hopcroft_DPDA.stepTransition, h, CPSP_Judge.stepTransition,
+        Hopcroft_DPDA_IDesc.toCPSP, Hopcroft_DPDA.toCPSP, h2]
       cases h3 : idesc.w with
       | nil =>
-        simp [h3]
+        simp only [Option.map_eq_none_iff]
         unfold Hopcroft_PreDPDA.transition
         match h4 : M.pda.4 (idesc.p, none, A) with
         | none =>
-          simp [h4]
+          simp only [h4]
         | some (p, α) =>
-          simp [h4]
+          simp only [h4, reduceCtorEq]
           unfold Hopcroft_DPDA.Δ at h2
-          simp at h2
+          simp only at h2
           rw [h4] at h2
-          simp at h2
+          simp only [reduceCtorEq] at h2
       | cons head tail =>
-        simp [h3]
+        simp only
         match hq : f head with
         | none =>
-          simp
+          simp only [Option.map_eq_none_iff]
           unfold Hopcroft_DPDA.Δ at h2
           match h4 : M.pda.transition (idesc.p, none, A) with
           | some (p, α) =>
-            simp [Option.map_some, Option.some.injEq]
-            simp at h2
+            simp only [reduceCtorEq]
+            simp only at h2
             rw [h4] at h2
-            simp at h2
+            simp only [reduceCtorEq] at h2
           | none =>
-            simp
+            simp only
             match h5 : M.pda.transition (idesc.p, some head, A) with
             | none =>
-              simp
+              simp only
             | some (p2, α2) =>
-              simp [h5]
+              simp only [reduceCtorEq]
               have h6 := M.deterministic
-              simp at h2
+              simp only at h2
               rw [h4] at h2
-              simp at h2
+              simp only [CPSP_Judge.step.injEq] at h2
               have h_head := congr_fun h2 head
-              simp [h5, hq] at h_head
+              simp only [h5, hq, reduceCtorEq] at h_head
         | some val =>
           unfold Hopcroft_DPDA_IDesc.toCPSP
-          simp
+          simp only [Option.map_eq_some_iff, CPSP_DPDA_IDesc.mk.injEq]
           match h4 : M.pda.transition (idesc.p, none, A) with
           | some (p, α) =>
-            simp
+            simp only [
+              Option.some.injEq, exists_eq_left',
+              List.cons_ne_self,
+              List.append_cancel_right_eq,
+              false_and, and_false
+            ]
             unfold Hopcroft_DPDA.Δ at h2
-            simp at h2
+            simp only at h2
             rw [h4] at h2
-            simp at h2
+            simp only [reduceCtorEq] at h2
           | none =>
-            simp
+            simp only
             match h5 : M.pda.transition (idesc.p, some head, A) with
             | some (p2, α2) =>
-              simp
+              simp only [Option.some.injEq, exists_eq_left', List.append_cancel_right_eq, true_and]
               unfold Hopcroft_DPDA.Δ at h2
-              simp at h2
+              simp only at h2
               rw [h4] at h2
-              simp at h2
+              simp only [CPSP_Judge.step.injEq] at h2
               have h_head := congr_fun h2 head
-              simp [h5, hq] at h_head
+              simp only [h5, hq, Option.some.injEq] at h_head
               apply pair_eq
               exact h_head
             | none =>
-              simp
+              simp only [reduceCtorEq, false_and, exists_false]
               unfold Hopcroft_DPDA.Δ at h2
-              simp at h2
+              simp only at h2
               rw [h4] at h2
-              simp at h2
+              simp only [CPSP_Judge.step.injEq] at h2
               have h_head := congr_fun h2 head
-              simp [h5, hq] at h_head
+              simp only [h5, hq, reduceCtorEq] at h_head
 
 lemma repeat_succ_outer {α} (f : α → α) (n : ℕ) (a : α) :
   Nat.repeat f (n + 1) a = f (Nat.repeat f n a) := by rfl
@@ -291,7 +306,7 @@ lemma repeat_succ_inner {α} (f : α → α) (n : ℕ) (a : α) :
 
 lemma decide_and {a: Bool} {b: Bool} {c: Bool} {d: Bool} (h2: b = d) (h1: a = c) :
  (a && b) = (c && d) := by
-  simp [h1, h2]
+  simp only [h1, h2]
 
 theorem Hopcroft_to_CPSP_preserves_semantics {Q S Γ} [Fintype Q] [DecidableEq Q] [Fintype S /- Σ -/] [Fintype Γ] [DecidableEq Γ]
   (M: Hopcroft_DPDA Q S Γ) (w: List S) (n: ℕ) :
@@ -313,7 +328,7 @@ theorem Hopcroft_to_CPSP_preserves_semantics {Q S Γ} [Fintype Q] [DecidableEq Q
       (Hopcroft_to_CPSP_preserves_semantics_single_step M)
       n
 
-    simp at h
+    simp only at h
     set k := (flipBind (CPSP_Judge.stepTransition M.toCPSP.transition) (some { p := M.toCPSP.q0, w := w, β := [] })) with hk2
     by_cases hk: ∃ a, (some (Hopcroft_DPDA_IDesc.toCPSP a) = k)
     · obtain ⟨ a, hk ⟩ := hk
@@ -324,31 +339,35 @@ theorem Hopcroft_to_CPSP_preserves_semantics {Q S Γ} [Fintype Q] [DecidableEq Q
       | some ⟨p2, w2, β2⟩ =>
         simp only [Hopcroft_DPDA_IDesc.toCPSP, Hopcroft_DPDA.toCPSP]
         rw [hk2] at hk
-        simp [flipBind, Hopcroft_DPDA_IDesc.toCPSP, Hopcroft_DPDA.toCPSP, CPSP_Judge.stepTransition] at hk
+        simp only [Hopcroft_DPDA_IDesc.toCPSP, flipBind, CPSP_Judge.stepTransition,
+          Hopcroft_DPDA.toCPSP, Option.some.injEq, CPSP_DPDA_IDesc.mk.injEq,
+          AugmentOneState.fromQ.injEq] at hk
         obtain ⟨ hp, hw, hβ ⟩ := hk
         rw [← hp, ← hw, ← hβ]
         have haa : (a = ⟨ a.p, a.w, a.β ⟩ ) := by rfl
         rw [← haa]
         rw [h2]
-        simp
+        simp only [Option.map_some, Finset.mem_image]
         apply decide_and
         · unfold Hopcroft_DPDA_IDesc.toCPSP
-          simp
+          simp only
         · unfold Hopcroft_DPDA_IDesc.toCPSP
-          simp
+          simp only [AugmentOneState.fromQ.injEq, exists_eq_right]
       | none =>
         simp only [Hopcroft_DPDA_IDesc.toCPSP, Hopcroft_DPDA.toCPSP]
         rw [hk2] at hk
-        simp [flipBind, Hopcroft_DPDA_IDesc.toCPSP, Hopcroft_DPDA.toCPSP, CPSP_Judge.stepTransition] at hk
+        simp only [Hopcroft_DPDA_IDesc.toCPSP, flipBind, CPSP_Judge.stepTransition,
+          Hopcroft_DPDA.toCPSP, Option.some.injEq, CPSP_DPDA_IDesc.mk.injEq,
+          AugmentOneState.fromQ.injEq] at hk
         obtain ⟨ hp, hw, hβ ⟩ := hk
         rw [← hp, ← hw, ← hβ]
         have haa : (a = ⟨ a.p, a.w, a.β ⟩ ) := by rfl
         rw [← haa]
         rw [h2]
-        simp
+        simp only [Option.map_none]
     · rw [hk2] at hk
       simp only [flipBind, Hopcroft_DPDA_IDesc.toCPSP, Hopcroft_DPDA.toCPSP, CPSP_Judge.stepTransition] at hk
       push_neg at hk
       have h3 := hk ⟨ M.pda.q0,  w,  [M.pda.z0] ⟩
       contrapose! h3
-      simp
+      simp only
