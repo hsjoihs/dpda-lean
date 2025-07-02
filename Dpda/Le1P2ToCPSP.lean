38af8a0b4a482c36d2c0d0a5a81c5aa155e096d5
hsjoihs <hirotaka_satou@dwango.co.jp>
2025-07-02 10:52:10 +0900

    Finish implementing Le1P2_DPDA.toCPSP

--- Dpda/Le1P2ToCPSP.lean
+++ Dpda/Le1P2ToCPSP.lean
@@ -1,6 +1,14 @@
 import Dpda.CharPopStringPush
 import Dpda.Le1PopLe1Push
 
+def foo {Q Γ} [DecidableEq Q] [DecidableEq Γ] (G : AugmentZ0 Γ) (wf_Γ : WobblyFn (AugmentZ0 Γ) (AugmentEpsilon Γ × Q)) : Option (List Γ × Q) :=
+  match wf_Γ with
+  | WobblyFn.noWant (γe, q) => match G with
+    | AugmentZ0.z0 => some (γe.toList, q)
+    | AugmentZ0.fromΓ G' => some (γe.toList ++ [G'], q)
+  | WobblyFn.want (f : AugmentZ0 Γ → Option (AugmentEpsilon Γ × Q)) =>
+    (f G).map fun (γe, q) => (γe.toList, q)
+
 def Le1P2_DPDA.toCPSP {Q S Γ} [DecidableEq Q] [DecidableEq Γ] (M: Le1P2_DPDA Q S Γ) : CPSP_DPDA Q S Γ :=
   let ⟨ q0, F, transition ⟩ := M
   let new_transition_curried : Q → AugmentZ0 Γ → CPSP_Judge Q S Γ
@@ -15,5 +23,10 @@ def Le1P2_DPDA.toCPSP {Q S Γ} [DecidableEq Q] [DecidableEq Γ] (M: Le1P2_DPDA Q
           match split with
           | .inl a => CPSP_Judge.immediate a
           | .inr f => CPSP_Judge.step f
-      | Le1P2_Judge.observeInput wf_S_wΓ => fun G => sorry
+      | Le1P2_Judge.observeInput
+          (delta_hat_p : WobblyFn S (WobblyFn (AugmentZ0 Γ) (AugmentEpsilon Γ × Q))) =>
+          fun G =>
+            match delta_hat_p with
+            | WobblyFn.noWant wfεΓ => CPSP_Judge.immediate (foo G wfεΓ)
+            | WobblyFn.want fS_wΓ => CPSP_Judge.step fun S => (fS_wΓ S >>= foo G)
   ⟨ q0, F, fun (q, Γz) =>  new_transition_curried q Γz ⟩
