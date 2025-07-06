8c39a1058f3a042ac7a0da88da3e6a93bc15a40e
hsjoihs <hirotaka_satou@dwango.co.jp>
2025-07-06 16:59:35 +0900

    How about this

--- Dpda/Main.lean
+++ Dpda/Main.lean
@@ -1,7 +1,6 @@
 universe u_
 
 structure Predet_DPDA (Q: Type u_) (S: Type u_) (Γ: Type u_) where
-  q0 : Q
   transition : Q → (Γ × Q) ⊕ Fin 42
 
 def Predet_DPDA.stepTransition {Q: Type u_} {S: Type u_} {Γ: Type u_}
@@ -10,10 +9,6 @@ def Predet_DPDA.stepTransition {Q: Type u_} {S: Type u_} {Γ: Type u_}
   : Option ((Q × List S × List Γ)) := match M.transition pwβ.1 with
   | Sum.inl (γ, q) => some ⟨q, pwβ.2.1, γ :: pwβ.2.2⟩
   | Sum.inr f => sorry
-
-structure Sipser_PreDPDA (Q S Γ) where
-  q0 : Q
-  transition : Q × Option S × Option Γ -> Option (Q × Option Γ)
 
 def exactly_one_some {α}
   (o1 : Option α)
@@ -27,33 +22,27 @@ def exactly_one_some {α}
   | _, _, _, _ => false
 
 structure Sipser_DPDA(Q S Γ) where
-  pda : Sipser_PreDPDA Q S Γ
+  pda_transition : Q × Option S × Option Γ -> Option (Q × Option Γ)
   deterministic : ∀ q a x,
     exactly_one_some
-      (pda.transition (q, some a, some x))
-      (pda.transition (q, some a, none))
-      (pda.transition (q, none, some x))
-      (pda.transition (q, none, none))
+      (pda_transition (q, some a, some x))
+      (pda_transition (q, some a, none))
+      (pda_transition (q, none, some x))
+      (pda_transition (q, none, none))
 
 def Sipser_DPDA.stepTransition {Q S Γ} (M: Sipser_DPDA Q S Γ) (pwβ: (Q × List S × List Γ)) : Option ((Q × List S × List Γ)) :=
-  match hεε : M.pda.transition (pwβ.1, none, none) with
+  match hεε : M.pda_transition (pwβ.1, none, none) with
   | some (r, y) => some ⟨ r, pwβ.2.1, y.toList ++ pwβ.2.2 ⟩
   | none =>
     match pwβ.2.1, pwβ.2.2 with
-    | [], [] => none
-    | [], x :: xs =>
-      match h2 : M.pda.transition (pwβ.1, none, some x) with
-      | some (r, y) => some ⟨ r, pwβ.2.1, y.toList ++ xs ⟩
-      | none => none
-    | a :: ws, [] =>
-      match h2 : M.pda.transition (pwβ.1, some a, none) with
-      | some (r, y) => some ⟨ r, ws, y.toList ++ pwβ.2.2 ⟩
-      | none => none
+    | [], [] => sorry
+    | [], x :: xs => sorry
+    | a :: ws, [] => sorry
     | a :: ws, x :: xs =>
       match
-        hax : (M.pda.transition (pwβ.1, some a, some x)),
-        haε : (M.pda.transition (pwβ.1, some a, none)),
-        hεx : (M.pda.transition (pwβ.1, none, some x)) with
+        hax : (M.pda_transition (pwβ.1, some a, some x)),
+        haε : (M.pda_transition (pwβ.1, some a, none)),
+        hεx : (M.pda_transition (pwβ.1, none, some x)) with
       | some (r, y), none, none => some ⟨ r, ws, y.toList ++ xs ⟩
       | none, some (r, y), none => some ⟨ r, pwβ.2.1, y.toList ++ xs ⟩
       | none, none, some (r, y) => some ⟨ r, ws, y.toList ++ pwβ.2.2 ⟩
@@ -64,20 +53,17 @@ def Sipser_DPDA.stepTransition {Q S Γ} (M: Sipser_DPDA Q S Γ) (pwβ: (Q × Lis
       | none, none, none       => False.elim <| by have h3 := M.deterministic pwβ.1 a x; simp [exactly_one_some, hεε, hax, haε, hεx] at h3
 
 def Predet_DPDA.toSipser {Q S Γ} [DecidableEq Q] (M: Predet_DPDA Q S Γ) : Sipser_DPDA (Option Q) S Γ :=
-  let ⟨ q0, dot_delta ⟩ := M
   let sipser_delta_curried : (Option Q) → Option Γ → Option S → Option (Option Q × Option Γ) :=
     fun p_ => match p_ with
      | none => sorry
      | some p =>
-       match dot_delta p with
+       match M.transition p with
         | Sum.inl (α, q) => sorry
         | Sum.inr fΓ_wS => sorry
   let is_deterministic := sorry
   ⟨
-    ⟨
-    some q0,
     fun (p, input_consumption, stack_consumption) => sipser_delta_curried p stack_consumption input_consumption
-    ⟩,
+    ,
     is_deterministic
   ⟩
 
